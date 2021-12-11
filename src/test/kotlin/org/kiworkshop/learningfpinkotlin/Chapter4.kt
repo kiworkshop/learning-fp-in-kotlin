9c1567d87e40042bcc9cf5f9f594a764297c7b89
anny0739 <ci7039@naver.com>
2021-12-11 21:46:33 +0900

    4 - 5

--- src/test/kotlin/org/kiworkshop/learningfpinkotlin/Chapter4.kt
+++ src/test/kotlin/org/kiworkshop/learningfpinkotlin/Chapter4.kt
@@ -15,4 +15,46 @@ class Chapter4 : StringSpec({
         isEvenOrOdd(2) shouldBe "2 is even"
         isEvenOrOdd(3) shouldBe "3 is odd"
     }
+
+    "연습문제 4-2" {
+        val func = { a: String, b: String, c: String -> a + b + c }
+        val f1 = func.partial1("Hello")
+        f1("World", "!") shouldBe "HelloWorld!"
+
+        val f2 = func.partial2("World")
+        f2("Hello", "!") shouldBe "HelloWorld!"
+
+        val f3 = func.partial3("!")
+        f3("Hello", "World") shouldBe "HelloWorld!"
+    }
+
+    "연습문제 4-3" {
+        fun max(a: Int) = { b: Int -> if (a > b) a else b }
+
+        max(2)(3) shouldBe 3
+        max(5)(3) shouldBe 5
+    }
+
+    "연습문제 4-4" {
+        val min = { a: Int, b: Int -> if (a < b) a else b }
+        min(1, 2) shouldBe 1
+
+        val minCurried = min.curried()
+        minCurried(1)(2) shouldBe 1
+    }
+
+    val maximum = { a: List<Int> -> a.maxOrNull() }
+    val power = { a: Int? -> if (a == null) 0 else a * a }
+
+    "연습문제 4-5" {
+        power(maximum(listOf(1, 2, 3))) shouldBe 9
+    }
+
+//    "연습문제 4-6" {
+//        infix fun <F, G, R> ((F) -> R).compose(g: (G) -> F): (G) -> R {
+//            return { gInput: G -> this(g(gInput)) }
+//        }
+//
+//        val composed = maximum compose power
+//    }
 })
