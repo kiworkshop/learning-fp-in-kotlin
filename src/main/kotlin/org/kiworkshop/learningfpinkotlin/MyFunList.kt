b79cf3d8f37926aec88dd180c07ae0944a7effa4
JongwonYoun <eatingcookieman@gmail.com>
2021-12-11 17:00:43 +0900

    MyFunList에서 List 생성을 도와주는 helper 메소드 작성

--- src/main/kotlin/org/kiworkshop/learningfpinkotlin/MyFunList.kt
+++ src/main/kotlin/org/kiworkshop/learningfpinkotlin/MyFunList.kt
@@ -34,3 +34,14 @@ fun <T> MyFunList<T>.getHead(): T = when (this) {
     Nil -> throw NoSuchElementException()
     is Cons -> head
 }
+
+fun <T> MyFunListOf(vararg elements: T): MyFunList<T> = elements.toMyFunList()
+fun <T> EmptyMyFunList(): MyFunList<T> = Nil
+
+// copyRange 함수를 이용하면 매번 새로운 Array를 생성하면서 함수형스럽게? 작성할 수 있다. 하지만 매번 객체 생성 비용이 추가된다.
+// 따라서 인덱스를 사용하는 꼬리 재귀 형태로 함수를 작성했다.
+// 다만 이 방법이 성능은 좋을지 몰라도 가독성은 copyRange 함수를 이용하는 것이 더 좋기 때문에 뭐가 더 좋은 선택일지 고민된다.
+tailrec fun <T> Array<out T>.toMyFunList(index: Int = this.size - 1, acc: MyFunList<T> = Nil): MyFunList<T> = when {
+    index < 0 -> acc
+    else -> this.toMyFunList(index - 1, acc.addHead(this[index]))
+}
--- src/test/kotlin/org/kiworkshop/learningfpinkotlin/chapter5/Practice3.kt
+++ src/test/kotlin/org/kiworkshop/learningfpinkotlin/chapter5/Practice3.kt
@@ -3,18 +3,18 @@ package org.kiworkshop.learningfpinkotlin.chapter5
 import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.FreeSpec
 import io.kotest.matchers.shouldBe
-import org.kiworkshop.learningfpinkotlin.MyFunList.Cons
-import org.kiworkshop.learningfpinkotlin.MyFunList.Nil
+import org.kiworkshop.learningfpinkotlin.EmptyMyFunList
+import org.kiworkshop.learningfpinkotlin.MyFunListOf
 import org.kiworkshop.learningfpinkotlin.getHead
 import org.kiworkshop.learningfpinkotlin.getTail
 
 class Practice3 : FreeSpec() {
     init {
         "리스트의 첫 번째 값을 가져오는 getHead 함수를 작성해 보자."{
-            val simpleList = Cons(1, Nil)
-            val intList = Cons(1, Cons(2, Cons(3, Cons(4, Cons(5, Nil)))))
-            val doubleList = Cons(1.0, Cons(2.0, Cons(3.0, Cons(4.0, Cons(5.0, Nil)))))
-            val emptyList = Nil
+            val simpleList = MyFunListOf(1)
+            val intList = MyFunListOf(1, 2, 3)
+            val doubleList = MyFunListOf(1.0, 2.0, 3.0)
+            val emptyList = EmptyMyFunList<Int>()
 
             intList.getHead() shouldBe 1
             doubleList.getHead() shouldBe 1.0
