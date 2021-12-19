71770b7d7ea8d88b44e718a65539248b76e730cf
Yul Jeon <notkhaki@gmail.com>
2021-12-19 21:50:12 +0900

    Solve 5-4

--- src/test/kotlin/org/kiworkshop/learningfpinkotlin/Chapter5.kt
+++ src/test/kotlin/org/kiworkshop/learningfpinkotlin/Chapter5.kt
@@ -1,5 +1,6 @@
 package org.kiworkshop.learningfpinkotlin
 
+import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
 import org.kiworkshop.learningfpinkotlin.FunList.*
@@ -30,6 +31,15 @@ fun <T> FunList<T>.getTail(): FunList<T> = when (this) {
     is Cons -> tail
 }
 
+tailrec fun <T> FunList<T>.filter(acc: FunList<T> = Nil, p: (T) -> Boolean): FunList<T> = when (this) {
+    Nil -> acc.reverse()
+    is Cons -> if (p(head)) {
+        tail.filter(acc.addHead(head), p)
+    } else {
+        tail.filter(acc, p)
+    }
+}
+
 class Chapter5 : StringSpec({
     "5-1" {
         val list: FunList<Int> = Cons(1, Cons(2, Cons(3, Cons(4, Cons(5, Nil)))))
@@ -49,5 +59,28 @@ class Chapter5 : StringSpec({
 
         list.getHead() shouldBe 1
         list.getTail().getHead() shouldBe 2
+    }
+
+    "5-4" {
+        tailrec fun <T> FunList<T>.drop(n: Int): FunList<T> = when (this) {
+            Nil -> throw NoSuchElementException()
+            is Cons -> when (n) {
+                0 -> this
+                1 -> tail
+                else -> tail.drop(n - 1)
+            }
+        }
+
+        shouldThrow<NoSuchElementException> { Nil.drop(0) }
+        Nil.appendTail(1).appendTail(2).appendTail(3)
+            .drop(1) shouldBe Cons(2, Cons(3, Nil))
+        Nil.appendTail(1).appendTail(2).appendTail(3)
+            .drop(2) shouldBe Cons(3, Nil)
+        Nil.appendTail(1).appendTail(2).appendTail(3)
+            .drop(3) shouldBe Nil
+        shouldThrow<NoSuchElementException> {
+            Nil.appendTail(1).appendTail(2).appendTail(3)
+                .drop(4) shouldBe Nil
+        }
     }
 })
