13c4dd1f1411a1bf359666f8aa59d8d0d5863109
Yul Jeon <notkhaki@gmail.com>
2021-12-20 17:25:59 +0900

    Refactor FunList

---
+++ src/main/kotlin/org/kiworkshop/learningfpinkotlin/FunList.kt
@@ -0,0 +1,76 @@
+package org.kiworkshop.learningfpinkotlin
+
+import org.kiworkshop.learningfpinkotlin.FunList.*
+
+sealed class FunList<out T> {
+    object Nil : FunList<Nothing>()
+    data class Cons<out T>(val head: T, val tail: FunList<T>) : FunList<T>()
+}
+
+fun <T> FunList<T>.addHead(head: T): FunList<T> = Cons(head, this)
+//fun <T> FunList<T>.appendTail(value: T): FunList<T> = when (this) {
+//    Nil -> Cons(value, Nil)
+//    is Cons -> Cons(head, tail.appendTail(value))
+//}
+
+tailrec fun <T> FunList<T>.reverse(acc: FunList<T> = Nil): FunList<T> = when (this) {
+    Nil -> acc
+    is Cons -> tail.reverse(acc.addHead(head))
+}
+
+tailrec fun <T> FunList<T>.appendTail(value: T, acc: FunList<T> = Nil): FunList<T> = when (this) {
+    Nil -> Cons(value, acc).reverse()
+    is Cons -> tail.appendTail(value, acc.addHead(head))
+}
+
+fun <T> FunList<T>.getTail(): FunList<T> = when (this) {
+    Nil -> throw NoSuchElementException()
+    is Cons -> tail
+}
+
+fun <T> FunList<T>.getHead(): T = when (this) {
+    Nil -> throw NoSuchElementException()
+    is Cons -> head
+}
+
+tailrec fun <T> FunList<T>.filter(acc: FunList<T> = Nil, p: (T) -> Boolean): FunList<T> = when (this) {
+    Nil -> acc.reverse()
+    is Cons -> if (p(head)) {
+        tail.filter(acc.addHead(head), p)
+    } else {
+        tail.filter(acc, p)
+    }
+}
+
+tailrec fun <T> FunList<T>.drop(n: Int): FunList<T> = when (this) {
+    Nil -> this
+    is Cons -> when {
+        n < 0 -> throw IllegalArgumentException()
+        n == 0 -> this
+        else -> tail.drop(n - 1)
+    }
+}
+
+tailrec fun <T> FunList<T>.dropWhile(p: (T) -> Boolean): FunList<T> = when (this) {
+    Nil -> this
+    is Cons -> when {
+        !p(head) -> this
+        else -> tail.dropWhile(p)
+    }
+}
+
+tailrec fun <T> FunList<T>.take(n: Int, acc: FunList<T> = Nil): FunList<T> = when (this) {
+    Nil -> acc.reverse()
+    is Cons -> when (n) {
+        0 -> acc.reverse()
+        else -> tail.take(n - 1, acc.addHead(head))
+    }
+}
+
+tailrec fun <T> FunList<T>.takeWhile(acc: FunList<T> = Nil, p: (T) -> Boolean): FunList<T> = when (this) {
+    Nil -> acc.reverse()
+    is Cons -> when {
+        !p(head) -> acc.reverse()
+        else -> tail.takeWhile(acc.addHead(head), p)
+    }
+}
--- src/test/kotlin/org/kiworkshop/learningfpinkotlin/Chapter5.kt
+++ src/test/kotlin/org/kiworkshop/learningfpinkotlin/Chapter5.kt
@@ -5,41 +5,6 @@ import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
 import org.kiworkshop.learningfpinkotlin.FunList.*
 
-sealed class FunList<out T> {
-    object Nil : FunList<Nothing>()
-    data class Cons<out T>(val head: T, val tail: FunList<T>) : FunList<T>()
-}
-
-fun <T> FunList<T>.addHead(head: T): FunList<T> = Cons(head, this)
-//fun <T> FunList<T>.appendTail(value: T): FunList<T> = when (this) {
-//    Nil -> Cons(value, Nil)
-//    is Cons -> Cons(head, tail.appendTail(value))
-//}
-
-tailrec fun <T> FunList<T>.reverse(acc: FunList<T> = Nil): FunList<T> = when (this) {
-    Nil -> acc
-    is Cons -> tail.reverse(acc.addHead(head))
-}
-
-tailrec fun <T> FunList<T>.appendTail(value: T, acc: FunList<T> = Nil): FunList<T> = when (this) {
-    Nil -> Cons(value, acc).reverse()
-    is Cons -> tail.appendTail(value, acc.addHead(head))
-}
-
-fun <T> FunList<T>.getTail(): FunList<T> = when (this) {
-    Nil -> throw NoSuchElementException()
-    is Cons -> tail
-}
-
-tailrec fun <T> FunList<T>.filter(acc: FunList<T> = Nil, p: (T) -> Boolean): FunList<T> = when (this) {
-    Nil -> acc.reverse()
-    is Cons -> if (p(head)) {
-        tail.filter(acc.addHead(head), p)
-    } else {
-        tail.filter(acc, p)
-    }
-}
-
 class Chapter5 : StringSpec({
     "5-1" {
         val list: FunList<Int> = Cons(1, Cons(2, Cons(3, Cons(4, Cons(5, Nil)))))
@@ -48,13 +13,8 @@ class Chapter5 : StringSpec({
     "5-2" {
         val list: FunList<Double> = Cons(1.0, Cons(2.0, Cons(3.0, Cons(4.0, Cons(5.0, Nil)))))
     }
-    
-    "5-3" {
-        fun <T> FunList<T>.getHead(): T = when (this) {
-            Nil -> throw NoSuchElementException()
-            is Cons -> head
-        }
 
+    "5-3" {
         val list = Cons(1, Cons(2, Cons(3, Nil)))
 
         list.getHead() shouldBe 1
@@ -62,15 +22,6 @@ class Chapter5 : StringSpec({
     }
 
     "5-4" {
-        tailrec fun <T> FunList<T>.drop(n: Int): FunList<T> = when (this) {
-            Nil -> this
-            is Cons -> when {
-                n < 0 -> throw IllegalArgumentException()
-                n == 0 -> this
-                else -> tail.drop(n - 1)
-            }
-        }
-
         Nil.appendTail(1).appendTail(2).appendTail(3)
             .drop(1) shouldBe Cons(2, Cons(3, Nil))
         Nil.appendTail(1).appendTail(2).appendTail(3)
@@ -82,14 +33,6 @@ class Chapter5 : StringSpec({
     }
 
     "5-5" {
-        tailrec fun <T> FunList<T>.dropWhile(p: (T) -> Boolean): FunList<T> = when (this) {
-            Nil -> this
-            is Cons -> when {
-                !p(head) -> this
-                else -> tail.dropWhile(p)
-            }
-        }
-
         Nil.appendTail(1).appendTail(2).appendTail(3)
             .dropWhile { it < 2 } shouldBe Cons(2, Cons(3, Nil))
         Nil.appendTail(1).appendTail(2).appendTail(3)
@@ -97,14 +40,6 @@ class Chapter5 : StringSpec({
     }
 
     "5-6" {
-        tailrec fun <T> FunList<T>.take(n: Int, acc: FunList<T> = Nil): FunList<T> = when (this) {
-            Nil -> acc.reverse()
-            is Cons -> when (n) {
-                0 -> acc.reverse()
-                else -> tail.take(n - 1, acc.addHead(head))
-            }
-        }
-
         Nil.appendTail(1).appendTail(2).appendTail(3)
             .take(0) shouldBe Nil
         Nil.appendTail(1).appendTail(2).appendTail(3)
@@ -114,14 +49,6 @@ class Chapter5 : StringSpec({
     }
 
     "5-7" {
-        tailrec fun <T> FunList<T>.takeWhile(acc: FunList<T> = Nil, p: (T) -> Boolean): FunList<T> = when (this) {
-            Nil -> acc.reverse()
-            is Cons -> when {
-                !p(head) -> acc.reverse()
-                else -> tail.takeWhile(acc.addHead(head), p)
-            }
-        }
-
         Nil.appendTail(1).appendTail(2).appendTail(3)
             .takeWhile { it < 1 } shouldBe Nil
         Nil.appendTail(1).appendTail(2).appendTail(3)
