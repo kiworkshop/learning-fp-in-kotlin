9a34926e73ec9c81676f080f1fdc5e3e8b8a5315
Yul Jeon <notkhaki@gmail.com>
2021-12-20 22:04:08 +0900

    Add comments to FunList for index in the book

--- src/main/kotlin/org/kiworkshop/learningfpinkotlin/FunList.kt
+++ src/main/kotlin/org/kiworkshop/learningfpinkotlin/FunList.kt
@@ -2,37 +2,46 @@ package org.kiworkshop.learningfpinkotlin
 
 import org.kiworkshop.learningfpinkotlin.FunList.*
 
+// Code 5-1
 sealed class FunList<out T> {
     object Nil : FunList<Nothing>()
     data class Cons<out T>(val head: T, val tail: FunList<T>) : FunList<T>()
 }
 
+// Code 5-3
 fun <T> FunList<T>.addHead(head: T): FunList<T> = Cons(head, this)
+
+// Code 5-4
 //fun <T> FunList<T>.appendTail(value: T): FunList<T> = when (this) {
 //    Nil -> Cons(value, Nil)
 //    is Cons -> Cons(head, tail.appendTail(value))
 //}
 
+// Code 5-6
 tailrec fun <T> FunList<T>.reverse(acc: FunList<T> = Nil): FunList<T> = when (this) {
     Nil -> acc
     is Cons -> tail.reverse(acc.addHead(head))
 }
 
+// Code 5-5
 tailrec fun <T> FunList<T>.appendTail(value: T, acc: FunList<T> = Nil): FunList<T> = when (this) {
     Nil -> Cons(value, acc).reverse()
     is Cons -> tail.appendTail(value, acc.addHead(head))
 }
 
+// Code 5-7
 fun <T> FunList<T>.getTail(): FunList<T> = when (this) {
     Nil -> throw NoSuchElementException()
     is Cons -> tail
 }
 
+// Exercise 5-3
 fun <T> FunList<T>.getHead(): T = when (this) {
     Nil -> throw NoSuchElementException()
     is Cons -> head
 }
 
+// Code 5-10
 tailrec fun <T> FunList<T>.filter(acc: FunList<T> = Nil, p: (T) -> Boolean): FunList<T> = when (this) {
     Nil -> acc.reverse()
     is Cons -> if (p(head)) {
@@ -42,6 +51,7 @@ tailrec fun <T> FunList<T>.filter(acc: FunList<T> = Nil, p: (T) -> Boolean): Fun
     }
 }
 
+// Exercise 5-4
 tailrec fun <T> FunList<T>.drop(n: Int): FunList<T> = when (this) {
     Nil -> this
     is Cons -> when {
@@ -51,6 +61,7 @@ tailrec fun <T> FunList<T>.drop(n: Int): FunList<T> = when (this) {
     }
 }
 
+// Exercise 5-5
 tailrec fun <T> FunList<T>.dropWhile(p: (T) -> Boolean): FunList<T> = when (this) {
     Nil -> this
     is Cons -> when {
@@ -59,6 +70,7 @@ tailrec fun <T> FunList<T>.dropWhile(p: (T) -> Boolean): FunList<T> = when (this
     }
 }
 
+// Exercise 5-6
 tailrec fun <T> FunList<T>.take(n: Int, acc: FunList<T> = Nil): FunList<T> = when (this) {
     Nil -> acc.reverse()
     is Cons -> when (n) {
@@ -67,6 +79,7 @@ tailrec fun <T> FunList<T>.take(n: Int, acc: FunList<T> = Nil): FunList<T> = whe
     }
 }
 
+// Exercise 5-7
 tailrec fun <T> FunList<T>.takeWhile(acc: FunList<T> = Nil, p: (T) -> Boolean): FunList<T> = when (this) {
     Nil -> acc.reverse()
     is Cons -> when {
@@ -75,11 +88,13 @@ tailrec fun <T> FunList<T>.takeWhile(acc: FunList<T> = Nil, p: (T) -> Boolean): 
     }
 }
 
+// Code 5-15
 tailrec fun <T, R> FunList<T>.map(acc: FunList<R> = Nil, f: (T) -> R): FunList<R> = when (this) {
     Nil -> acc.reverse()
     is Cons -> tail.map(acc.addHead(f(head)), f)
 }
 
+// Code 5-17
 fun <T> funListOf(vararg elements: T): FunList<T> = elements.toFunList()
 
 private fun <T> Array<out T>.toFunList(): FunList<T> = when {
@@ -87,54 +102,78 @@ private fun <T> Array<out T>.toFunList(): FunList<T> = when {
     else -> Cons(this[0], this.copyOfRange(1, this.size).toFunList())
 }
 
+// Exercise 5-8
 tailrec fun <T, R> FunList<T>.indexedMap(index: Int = 0, acc: FunList<R> = Nil, f: (Int, T) -> R): FunList<R> =
     when (this) {
         Nil -> acc.reverse()
         is Cons -> tail.indexedMap(index + 1, acc.addHead(f(index, head)), f)
     }
 
+// Code 5-18
+//fun sum(list: FunList<Int>): Int = when (list) {
+//    Nil -> 0
+//    is Cons -> list.head + sum(list.tail)
+//}
+
+// Code 5-19
 tailrec fun <T, R> FunList<T>.foldLeft(acc: R, f: (R, T) -> R): R = when (this) {
     Nil -> acc
     is Cons -> tail.foldLeft(f(acc, head), f)
 }
 
+// Code 5-20
+fun sumByFoldLeft(list: FunList<Int>): Int = list.foldLeft(0) { acc, x -> acc + x }
+
+// Code 5-21
+fun FunList<Int>.sum(): Int = foldLeft(0) { acc, x -> acc + x }
+
+// Exercise 5-9
 fun FunList<Int>.maximumByFoldLeft(acc: Int = this.getHead()): Int = this.foldLeft(acc) { acc: Int, x ->
     if (acc < x) x else acc
 }
 
+// Exercise 5-10
 fun <T> FunList<T>.filterByFoldLeft(acc: FunList<T> = Nil, p: (T) -> Boolean): FunList<T> =
     this.foldLeft(acc) { acc: FunList<T>, x ->
         if (p(x)) acc.appendTail(x) else acc
     }
 
+// Code 5-25
 fun <T, R> FunList<T>.foldRight(acc: R, f: (T, R) -> R): R = when (this) {
     Nil -> acc
     is Cons -> f(head, tail.foldRight(acc, f))
 }
 
+// Exercise 5-11
 fun <T> FunList<T>.reverseByFoldRight(): FunList<T> = this.foldRight(Nil) { x, acc: FunList<T> ->
     acc.appendTail(x)
 }
 
+// Exercise 5-12
 fun <T> FunList<T>.filterByFoldRight(p: (T) -> Boolean): FunList<T> = this.foldRight(Nil) { x, acc: FunList<T> ->
     if (p(x)) acc.addHead(x) else acc
 }
 
+// Exercise 5-13
 tailrec fun <T, R> FunList<T>.zip(other: FunList<R>, acc: FunList<Pair<T, R>> = Nil): FunList<Pair<T, R>> = when {
     this === Nil || other === Nil -> acc.reverse()
     else -> this.getTail().zip(other.getTail(), acc.addHead(Pair(this.getHead(), other.getHead())))
 }
 
-tailrec fun <T1, T2, R> FunList<T1>.zipWith(f: (T1, T2) -> R, list: FunList<T2>, acc: FunList<R> = Nil): FunList<R> = when {
-    this === Nil || list === Nil -> acc.reverse()
-    else -> getTail().zipWith(f, list.getTail(), acc.addHead(f(getHead(), list.getHead())))
-}
+// Code 5-30
+tailrec fun <T1, T2, R> FunList<T1>.zipWith(f: (T1, T2) -> R, list: FunList<T2>, acc: FunList<R> = Nil): FunList<R> =
+    when {
+        this === Nil || list === Nil -> acc.reverse()
+        else -> getTail().zipWith(f, list.getTail(), acc.addHead(f(getHead(), list.getHead())))
+    }
 
+// Exercise 5-14
 fun <T, R> FunList<T>.associate(f: (T) -> Pair<T, R>): Map<T, R> = when {
     this === Nil -> mapOf()
     else -> mapOf(f(getHead())) + getTail().associate(f)
 }
 
+// Exercise 5-15
 fun <T, K> FunList<T>.groupBy(f: (T) -> K): Map<K, FunList<T>> = when {
     this === Nil -> mapOf()
     else -> mapOf(f(getHead()) to funListOf(getHead())) + getTail().groupBy(f)
