124ca3983e105a76507442c825da3aa09beeda55
JongwonYoun <eatingcookieman@gmail.com>
2021-12-20 23:09:13 +0900

    연습문제 4-8 람다 마지막 파라미터로 변경

--- src/test/kotlin/org/kiworkshop/learningfpinkotlin/chapter4/Practice8.kt
+++ src/test/kotlin/org/kiworkshop/learningfpinkotlin/chapter4/Practice8.kt
@@ -9,20 +9,21 @@ class Practice8 : FreeSpec() {
     private fun <T> Sequence<T>.tail() = drop(1)
 
     private tailrec fun <T> takeWhile(
-        predicate: (T) -> Boolean,
         sequence: Sequence<T>,
-        acc: List<T> = emptyList()
+        acc: List<T> = emptyList(),
+        predicate: (T) -> Boolean
     ): List<T> = when {
         sequence.none() -> acc
         !predicate(sequence.head()) -> acc
-        else -> takeWhile(predicate, sequence.tail(), acc + sequence.head())
+        else -> takeWhile(sequence.tail(), acc + sequence.head(), predicate)
     }
 
     init {
         "연습문제 4-7에서 작성한 takeWhile을 수정하여, 무한대를 입력받을 수 있는 takeWhile을 꼬리 재귀로 작성해 보자."{
-            takeWhile({ a: Int -> a < 3 }, generateSequence(1) { it + 1 }) shouldContainInOrder listOf(1, 2)
-            takeWhile({ a: Int -> a < 3 }, generateSequence(null as Int?) { it + 1 }) shouldHaveSize 0
-            takeWhile({ a: Int -> a % 13 != 0 }, generateSequence(1) { it + 1 }) shouldContainInOrder (1..12).toList()
+            var b: Sequence<Int>
+            takeWhile(generateSequence(1) { it + 1 }) { it < 3 } shouldContainInOrder listOf(1, 2)
+            takeWhile(generateSequence(null as Int?) { it + 1 }) { it < 3 } shouldHaveSize 0
+            takeWhile(generateSequence(1) { it + 1 }) { it % 13 != 0 } shouldContainInOrder (1..12).toList()
         }
     }
 }
