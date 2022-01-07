b5e8231daa0c868e32f370ab3e14f4f0ff883945
korECM <eatingcookieman@gmail.com>
2022-01-08 00:39:21 +0900

    연습문제 8-4 풀이

---
+++ src/main/kotlin/org/kiworkshop/learningfpinkotlin/MyTree.kt
@@ -0,0 +1,32 @@
+package org.kiworkshop.learningfpinkotlin
+
+sealed class MyTree<out A> : Functor<A> {
+    abstract override fun <B> fmap(f: (A) -> B): Functor<B>
+
+    companion object
+}
+
+data class MyNode<out A>(val value: A, val forest: List<MyNode<A>> = emptyList()) : MyTree<A>() {
+    override fun toString(): String = if(forest.isEmpty()) "$value" else "$value $forest"
+
+    override fun <B> fmap(f: (A) -> B): MyNode<B> = MyNode(f(value), forest.map { it.fmap(f) })
+}
+
+fun <A> MyTree.Companion.pure(value: A) = MyNode(value)
+
+infix fun <A, B> MyNode<(A) -> B>.apply(node: MyNode<A>): MyNode<B> = MyNode(
+    value(node.value),
+    node.forest.map { it.fmap(value) } + forest.map { it apply node }
+)
+
+fun main() {
+    val tree = MyNode(1, listOf(MyNode(2), MyNode(3)))
+    println(tree.fmap { it * 2 })
+    println(MyTree.pure({ x: Int -> x * 2 }) apply tree)
+
+    println(
+        MyTree.pure({ x: Int, y: Int -> x * y }.curried())
+                apply MyNode(1, listOf(MyNode(2), MyNode(3)))
+                apply MyNode(4, listOf(MyNode(5), MyNode(6)))
+    )
+}
---
+++ src/test/kotlin/org/kiworkshop/learningfpinkotlin/chpater8/Practice4.kt
@@ -0,0 +1,28 @@
+package org.kiworkshop.learningfpinkotlin.chpater8
+
+import io.kotest.core.spec.style.FreeSpec
+import io.kotest.matchers.shouldBe
+import org.kiworkshop.learningfpinkotlin.MyNode
+import org.kiworkshop.learningfpinkotlin.MyTree
+import org.kiworkshop.learningfpinkotlin.apply
+import org.kiworkshop.learningfpinkotlin.curried
+import org.kiworkshop.learningfpinkotlin.pure
+
+class Practice4 : FreeSpec() {
+    init {
+        "다음가 같이 두 트리를 반대의 순서로 적용했을 때는 어떤 트리가 완성될지 예상해 보고, 테스트해 보자."{
+            (MyTree.pure({ x: Int, y: Int -> x * y }.curried())
+                    apply MyNode(4, listOf(MyNode(5), MyNode(6)))
+                    apply MyNode(1, listOf(MyNode(2), MyNode(3)))
+                    ) shouldBe MyNode(
+                4,
+                listOf(
+                    MyNode(8),
+                    MyNode(12),
+                    MyNode(5, listOf(MyNode(10), MyNode(15))),
+                    MyNode(6, listOf(MyNode(12), MyNode(18)))
+                )
+            )
+        }
+    }
+}
