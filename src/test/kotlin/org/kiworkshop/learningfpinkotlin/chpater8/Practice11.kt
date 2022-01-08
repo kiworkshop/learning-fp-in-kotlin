7397d1cf49b15e8dcee518ab9b5bb13040ce9bd4
korECM <eatingcookieman@gmail.com>
2022-01-08 17:12:38 +0900

    연습문제 8-11 풀이

---
+++ src/test/kotlin/org/kiworkshop/learningfpinkotlin/chpater8/Practice11.kt
@@ -0,0 +1,19 @@
+package org.kiworkshop.learningfpinkotlin.chpater8
+
+import io.kotest.core.spec.style.FreeSpec
+import io.kotest.matchers.shouldBe
+import org.kiworkshop.learningfpinkotlin.chpater8.Practice3.FunList
+import org.kiworkshop.learningfpinkotlin.of
+
+class Practice11 : FreeSpec() {
+    init {
+        "연습문제 8-3에서 만든 리스트 애플리케이티브 펑터가 pure(function) apply af = af.fmap(function)을 만족하는지 확인해 보자."{
+            val x = 15
+            val function = { y : Int -> y * 2}
+            val listAf = FunList.pure(x)
+            val leftList = FunList.pure(function) apply listAf
+            val rightList = listAf.fmap(function)
+            leftList.toString() shouldBe rightList.toString()
+        }
+    }
+}
