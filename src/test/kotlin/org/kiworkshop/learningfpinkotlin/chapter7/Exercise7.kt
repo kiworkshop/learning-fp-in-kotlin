9bbc4af1c19436a822d0bd92de433e543f70c124
Hyenakim <gpsk1202@gmail.com>
2022-01-08 16:32:11 +0900

    7장 funList 수정

--- src/test/kotlin/org/kiworkshop/learningfpinkotlin/chapter7/Exercise7.kt
+++ src/test/kotlin/org/kiworkshop/learningfpinkotlin/chapter7/Exercise7.kt
@@ -3,10 +3,6 @@ package org.kiworkshop.learningfpinkotlin.chapter7
 import io.kotest.core.spec.style.AnnotationSpec
 import io.kotest.matchers.shouldBe
 import org.kiworkshop.learningfpinkotlin.chapter4.compose
-import org.kiworkshop.learningfpinkotlin.chapter7.FunList.Cons
-import org.kiworkshop.learningfpinkotlin.chapter7.FunList.Nil
-import org.kiworkshop.learningfpinkotlin.chapter7.FunList.Nil.first
-import org.kiworkshop.learningfpinkotlin.chapter7.FunList.Nil.size
 
 class Exercise7 : AnnotationSpec() {
     @Test
@@ -14,7 +10,7 @@ class Exercise7 : AnnotationSpec() {
         val funList: FunList<Int> = Cons(1, Cons(2, Cons(3, Nil)))
 
         funList.fmap { it * 3 } shouldBe
-            Cons(3, Cons(6, Cons(9, Nil)))
+                Cons(3, Cons(6, Cons(9, Nil)))
         funList.first() shouldBe 1
         funList.size() shouldBe 3
 
