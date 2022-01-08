cb1a420bf1b471ebcb1bc741be59502790f76c0d
korECM <eatingcookieman@gmail.com>
2022-01-08 17:08:24 +0900

    연습문제 8-10 풀이

--- src/main/kotlin/org/kiworkshop/learningfpinkotlin/Chpater8Common.kt
+++ src/main/kotlin/org/kiworkshop/learningfpinkotlin/Chpater8Common.kt
@@ -1,3 +1,5 @@
 package org.kiworkshop.learningfpinkotlin
 
 fun <P1, P2, P3> compose() = { f: (P2) -> P3, g: (P1) -> P2, v: P1 -> f(g(v)) }
+
+fun <T, R> of(value: T) = { f: (T) -> R -> f(value)}
---
+++ src/test/kotlin/org/kiworkshop/learningfpinkotlin/chpater8/Practice10.kt
@@ -0,0 +1,18 @@
+package org.kiworkshop.learningfpinkotlin.chpater8
+
+import io.kotest.core.spec.style.FreeSpec
+import io.kotest.matchers.shouldBe
+import org.kiworkshop.learningfpinkotlin.chpater8.Practice3.FunList
+import org.kiworkshop.learningfpinkotlin.of
+
+class Practice10 : FreeSpec() {
+    init {
+        "연습문제 8-3에서 만든 리스트 애플리케이티브 펑터가 교환 법칙을 만족하는지 확인해 보자."{
+            val x = 15
+            val listAf = FunList.pure { y: Int -> y * 2 }
+            val leftList = listAf apply FunList.pure(x)
+            val rightList = FunList.pure(of<Int, Int>(x)) apply listAf
+            leftList.toString() shouldBe rightList.toString()
+        }
+    }
+}
