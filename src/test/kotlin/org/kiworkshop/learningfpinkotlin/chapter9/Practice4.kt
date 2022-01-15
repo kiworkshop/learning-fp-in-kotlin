50d8e32ae3920373bda87e73966768b08e050ca1
korECM <eatingcookieman@gmail.com>
2022-01-15 19:53:41 +0900

    연습문제 9-4 풀이

---
+++ src/test/kotlin/org/kiworkshop/learningfpinkotlin/chapter9/Practice4.kt
@@ -0,0 +1,20 @@
+package org.kiworkshop.learningfpinkotlin.chapter9
+
+import io.kotest.core.spec.style.FreeSpec
+import io.kotest.matchers.shouldBe
+import org.kiworkshop.learningfpinkotlin.AllMonoid
+
+class Practice4 : FreeSpec() {
+    init {
+        "All 모노이드가 모노이드의 법칙을 만족하는지 테스트해 보자" {
+            val x = false
+            val y = true
+            val z = false
+            AllMonoid().run {
+                mappend(mempty(), x) shouldBe x
+                mappend(x, mempty()) shouldBe x
+                mappend(mappend(x, y), z) shouldBe mappend(x, mappend(y, z))
+            }
+        }
+    }
+}
