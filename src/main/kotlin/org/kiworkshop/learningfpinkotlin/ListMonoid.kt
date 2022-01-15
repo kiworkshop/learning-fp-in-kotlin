0227269978c50cce7ef36affa35b3153d953afca
korECM <eatingcookieman@gmail.com>
2022-01-15 20:06:56 +0900

    ListMonoid class로 수정

--- src/main/kotlin/org/kiworkshop/learningfpinkotlin/ListMonoid.kt
+++ src/main/kotlin/org/kiworkshop/learningfpinkotlin/ListMonoid.kt
@@ -1,12 +1,10 @@
 package org.kiworkshop.learningfpinkotlin
 
-object ListMonoid {
-    fun <T> monoid() = object : Monoid<FunList<T>> {
-        override fun mempty(): FunList<T> = funListOf()
+class ListMonoid<T> : Monoid<FunList<T>> {
+    override fun mempty(): FunList<T> = funListOf()
 
-        override fun mappend(m1: FunList<T>, m2: FunList<T>): FunList<T> = when (m1) {
-            is FunList.Nil -> m2
-            is FunList.Cons -> m1 concat m2
-        }
+    override fun mappend(m1: FunList<T>, m2: FunList<T>): FunList<T> = when (m1) {
+        is FunList.Nil -> m2
+        is FunList.Cons -> m1 concat m2
     }
 }
