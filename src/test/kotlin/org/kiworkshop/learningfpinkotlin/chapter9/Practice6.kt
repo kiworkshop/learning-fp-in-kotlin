79ff25a218e92a6ab351dc3c36a48e92f9e6473f
korECM <eatingcookieman@gmail.com>
2022-01-15 20:04:24 +0900

    연습문제 9-6 풀이

---
+++ src/test/kotlin/org/kiworkshop/learningfpinkotlin/chapter9/Practice6.kt
@@ -0,0 +1,22 @@
+package org.kiworkshop.learningfpinkotlin.chapter9
+
+import io.kotest.core.spec.style.FreeSpec
+import io.kotest.matchers.shouldBe
+import org.kiworkshop.learningfpinkotlin.AllMonoid
+import org.kiworkshop.learningfpinkotlin.funListOf
+import org.kiworkshop.learningfpinkotlin.mconcat
+
+class Practice6 : FreeSpec() {
+    init {
+        "All 모노이드의 mconcat 함수를 테스트해 보자. 입력이 [true, true, true], [false, false, false], [true, false, true]라면 어떤 결과가 나오는가?" {
+            val input1 = funListOf(true, true, true)
+            val input2 = funListOf(false, false, false)
+            val input3 = funListOf(true, false, true)
+            AllMonoid().run {
+                mconcat(input1) shouldBe true
+                mconcat(input2) shouldBe false
+                mconcat(input3) shouldBe false
+            }
+        }
+    }
+}
