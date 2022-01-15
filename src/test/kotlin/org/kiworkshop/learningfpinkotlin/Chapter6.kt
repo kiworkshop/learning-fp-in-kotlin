0aac819860a86369ceeb991b0fcc49a2273b8cb9
김성훈 <seonghun127@gmail.com>
2022-01-15 21:50:54 +0900

    코드 리펙토링

--- src/test/kotlin/org/kiworkshop/learningfpinkotlin/Chapter6.kt
+++ src/test/kotlin/org/kiworkshop/learningfpinkotlin/Chapter6.kt
@@ -9,7 +9,7 @@ class Chapter6 : FunSpec({
     context("P.169 연습 문제") {
         test(
             "6-1. 재귀적 자료구조를 확룡아형 이진 트리를 만들어 보자." +
-                "여기서 이진 트리는 균형잡힌 트리가 아니고 일반적인 트리다. 트리의 정의는 다음과 같다."
+                    "여기서 이진 트리는 균형잡힌 트리가 아니고 일반적인 트리다. 트리의 정의는 다음과 같다."
         ) {
             /**
              * 모든 노드는 하위 노드가 없거나(EmptyTree) 최대 두 개의 하위 노드(Node)를 가진다.
@@ -39,8 +39,8 @@ class Chapter6 : FunSpec({
 
         test(
             "6-2. 연습문제 6-1에서 만든 이진 트리에 노드를 추가하는 insert 함수를 Tree의 확장 함수로 만들어보자." +
-                "이때 왼쪽 하위 노드의 값은 오른쪽 하위 노드의 값보다 항상 작아야한다. 단, 값을 비교하기 위해서넌 T가 항상 Comparable 속성을 가지고 있어야한다." +
-                "여기서 문제의 복잡도를 낮추기 위해 입력 타입을 Int로 제한한다."
+                    "이때 왼쪽 하위 노드의 값은 오른쪽 하위 노드의 값보다 항상 작아야한다. 단, 값을 비교하기 위해서넌 T가 항상 Comparable 속성을 가지고 있어야한다." +
+                    "여기서 문제의 복잡도를 낮추기 위해 입력 타입을 Int로 제한한다."
         ) {
             fun BinaryTree<Int>.insert(element: Int): BinaryTree<Int> = when (this) {
                 is EmptyNode -> Node(element)
@@ -51,8 +51,7 @@ class Chapter6 : FunSpec({
                 }
             }
 
-            val binaryTree = EmptyNode
-            binaryTree.insert(4)
+            EmptyNode.insert(4)
                 .insert(2)
                 .insert(6)
                 .insert(1)
@@ -62,26 +61,30 @@ class Chapter6 : FunSpec({
                 .toString() shouldBe "(N 4 (N 2 (N 1 E E) (N 3 E E)) (N 6 (N 5 E E) (N 7 E E)))"
         }
 
-        xtest("6-3. 연습문제 6-2에서 작성한 insert 코드를 100,000번 이상 연속해서 insert 해보자.") {
-            val binaryTree = EmptyNode
-
-            fun insertBy1_000_000_000() {
-                for (i in 0..1_000_000_000) {
-                    binaryTree.insert(i)
+        test("6-3. 연습문제 6-2에서 작성한 insert 코드를 100,000번 이상 연속해서 insert 해보자.") {
+            shouldThrowExactly<StackOverflowError> {
+                (1..1_000_000_000).fold(EmptyNode as BinaryTree<Int>) { acc, element ->
+                    acc.insert(element)
                 }
             }
-
-            // stack overflow 발생하지 않음 ㅇㅅㅇ
-            shouldThrowExactly<StackOverflowError> { insertBy1_000_000_000() }
         }
 
         test("6-4. StackOverFlowError가 발생하지 않도록 insertTailrec을 작성해보자.") {
             fun BinaryTree<Int>.insertTailrec(element: Int): BinaryTree<Int> = TODO()
+
+            EmptyNode.insertTailrec(4)
+                .insertTailrec(2)
+                .insertTailrec(6)
+                .insertTailrec(1)
+                .insertTailrec(3)
+                .insertTailrec(5)
+                .insertTailrec(7)
+                .toString() shouldBe "(N 4 (N 2 (N 1 E E) (N 3 E E)) (N 6 (N 5 E E) (N 7 E E)))"
         }
 
         test(
             "6-5. 연습문제 6-1에서 만든 이진 트리에 어떤 노드가 존재하는지 확인하는 contains 함수를 추가해보자." +
-                "문제의 복잡도를 낮추기 위해 입력 타입을 Int로 제한한다."
+                    "문제의 복잡도를 낮추기 위해 입력 타입을 Int로 제한한다."
         ) {
             tailrec fun BinaryTree<Int>.contains(element: Int): Boolean = when (this) {
                 is EmptyNode -> false
